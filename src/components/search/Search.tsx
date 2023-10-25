d8502ac9cb783094a577bd8da5bc2a609e085e49
onfranciis <onukwuf@gmail.com>
2023-10-25 18:16:29 +0100

    feat: create and style Search

---
+++ src/components/search/Search.tsx
@@ -0,0 +1,26 @@
+import { ChangeEvent } from "react";
+import "./Search.scss";
+
+interface ISearchProp {
+  setInput: (data: string) => void;
+  Input: string;
+}
+
+const Search = ({ setInput, Input }: ISearchProp) => {
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setInput(e.target.value);
+  };
+
+  return (
+    <div className="Search">
+      <input
+        type="text"
+        placeholder="Search your notes..."
+        value={Input}
+        onChange={handleChange}
+      />
+    </div>
+  );
+};
+
+export default Search;
