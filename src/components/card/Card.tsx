3d833d9fed59fe9b853021f2a168ada6bc31a9f4
onfranciis <onukwuf@gmail.com>
2023-10-27 08:41:28 +0100

    feat: add Preveiw to Card

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -1,14 +1,64 @@
+import { useEffect, useState } from "react";
+import { FaChevronLeft, FaChevronRight } from "react-icons/fa6";
 import "./Card.scss";
 
 interface ICardProps {
-  Data: { Title: string; Body: string };
+  Data: { Title: string; Body: string }[];
+  Index: number;
 }
 
-const Card = ({ Data }: ICardProps) => {
+const Card = ({ Data, Index }: ICardProps) => {
+  const [targetIndex, setTargetIndex] = useState(Index);
+  const [selected, setSelected] = useState(false);
+
+  const Iterate = (type: "Prev" | "Next") => {
+    if (type == "Prev") {
+      if (targetIndex !== 0) {
+        setTargetIndex(targetIndex - 1);
+      }
+    } else if (type == "Next") {
+      if (targetIndex !== Data.length - 1) {
+        setTargetIndex(targetIndex + 1);
+      }
+    }
+  };
+
+  useEffect(() => {
+    setTargetIndex(Index);
+  }, [selected]);
+
   return (
-    <div className="Card">
-      <p className="title">{Data.Title}</p>
-      <p className="body">{Data.Body} </p>
+    <div
+      className={`PreviewParent ${selected && "Preview"}`}
+      onClick={() => setSelected(false)}
+    >
+      <div
+        className={`Card ${selected && "Preview"}`}
+        onClick={(e) => {
+          e.stopPropagation();
+          setSelected(true);
+        }}
+      >
+        <div>
+          <p className="title">{Data[selected ? targetIndex : Index]?.Title}</p>
+
+          <div className={`Tools ${selected && "Preview"}`}>
+            <button onClick={() => Iterate("Prev")}>
+              <FaChevronLeft />
+            </button>
+
+            <p>{`${targetIndex + 1}/${Data.length}`}</p>
+
+            <button onClick={() => Iterate("Next")}>
+              <FaChevronRight />
+            </button>
+          </div>
+        </div>
+
+        <pre className="body">
+          {Data[selected ? targetIndex : Index]?.Body.trim()}{" "}
+        </pre>
+      </div>
     </div>
   );
 };
