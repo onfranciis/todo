4a9b1e73c29e7d7a68c063b9eb5af074fe3f3008
onfranciis <onukwuf@gmail.com>
2023-10-28 12:14:58 +0100

    feat: add types for Mode and handleToolsArg

--- src/types/index.ts
+++ src/types/index.ts
@@ -2,4 +2,9 @@ export interface IList {
   Title: string;
   Body: string;
   Time: string;
+  Id: string;
 }
+
+export type Mode = "Edit" | "Preview";
+
+export type handleToolsArg = Mode | "Prev" | "Next" | "Copy" | "Update";
